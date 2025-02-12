b301a81a0c5409aeddebc57abd50cecede3af273
Aram Peres <6775216+aramperes@users.noreply.github.com>
2025-02-12 12:33:45 -0500

    fmt

--- tf/vault.tf
+++ tf/vault.tf
@@ -21,34 +21,34 @@ EOT
 // Creates an entity to represent the user in Vault. The entity has a list of MinIO policies attached to it.
 // An entity can have multiple aliases, which are used to authenticate the entity.
 resource "vault_identity_entity" "user1" {
-  name      = "user1"
-  policies  = []
-  metadata  = {
+  name     = "user1"
+  policies = []
+  metadata = {
     minio_policies = "readonly"
   }
 }
 
 // Create an alias for the user in the userpass backend, and tie it to the entity we created above.
 resource "vault_identity_entity_alias" "test" {
-  name            = "user1"
-  mount_accessor  = vault_auth_backend.userpass.accessor
-  canonical_id    = vault_identity_entity.user1.id
+  name           = "user1"
+  mount_accessor = vault_auth_backend.userpass.accessor
+  canonical_id   = vault_identity_entity.user1.id
 }
 
 // Next, we create an "assigmment" which is used to determine which entities are allowed to access a particular client (MinIO).
 resource "vault_identity_oidc_assignment" "minio_users" {
-  name       = "minio_users"
+  name = "minio_users"
   entity_ids = [
     vault_identity_entity.user1.id,
   ]
-  group_ids  = []
+  group_ids = []
 }
 
 // We create the OIDC client for MinIO. This client will be used by MinIO to authenticate users.
 // A client ID and client secret will be available in the Vault UI to be pasted into the MinIO configuration.
 resource "vault_identity_oidc_client" "minio" {
-  name          = "minio"
-  key           = vault_identity_oidc_key.minio.name
+  name = "minio"
+  key  = vault_identity_oidc_key.minio.name
   redirect_uris = [
     "http://localhost:9001/oauth_callback"
   ]
@@ -87,9 +87,9 @@ resource "vault_identity_oidc_scope" "minio_policy" {
 // Create the OIDC provider for MinIO. This provider will be used by MinIO to authenticate users.
 // Config URL: http://vault:8200/v1/identity/oidc/provider/minio/.well-known/openid-configuration
 resource "vault_identity_oidc_provider" "minio" {
-  name = "minio"
+  name          = "minio"
   https_enabled = false
-  issuer_host = "vault:8200"
+  issuer_host   = "vault:8200"
   allowed_client_ids = [
     vault_identity_oidc_client.minio.client_id
   ]
